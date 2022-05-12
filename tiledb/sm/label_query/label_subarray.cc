e875ac2d7cec89966a2ce35256626d88a95b6456
Julia Dark <julia@tiledb.com>
2022-05-12 14:56:49 -0400

    Adds labelled subarray [skip ci]

---
+++ tiledb/sm/label_query/label_subarray.cc
@@ -0,0 +1,196 @@
+#include "tiledb/sm/label_query/label_subarray.h"
+#include "tiledb/common/logger.h"
+#include "tiledb/sm/array/array.h"
+#include "tiledb/sm/config/config.h"
+#include "tiledb/sm/enums/layout.h"
+#include "tiledb/sm/stats/stats.h"
+#include "tiledb/sm/storage_manager/storage_manager.h"
+
+using namespace tiledb::common;
+
+namespace tiledb::sm {
+
+AxisSubarray::AxisSubarray(
+    const std::string& label_name,
+    const std::string& internal_label_name,
+    const std::string& internal_index_name,
+    const Array* array,
+    const Layout layout,
+    stats::Stats* stats,
+    shared_ptr<Logger> logger,
+    const bool coalesce_ranges)
+    : subarray(array, layout, stats, logger, coalesce_ranges, nullptr)
+    , label_name_(label_name)
+    , internal_label_name_(internal_label_name)
+    , internal_index_name_(internal_index_name) {
+}
+
+LabelledSubarray::LabelledSubarray(
+    const Array* array,
+    stats::Stats* const parent_stats,
+    shared_ptr<Logger> logger,
+    const bool coalesce_ranges,
+    StorageManager* storage_manager)
+    : LabelledSubarray(
+          array,
+          Layout::UNORDERED,
+          parent_stats,
+          logger,
+          coalesce_ranges,
+          storage_manager) {
+}
+
+LabelledSubarray::LabelledSubarray(
+    const Array* const array,
+    const Layout layout,
+    stats::Stats* const parent_stats,
+    shared_ptr<Logger> logger,
+    const bool coalesce_ranges,
+    StorageManager* storage_manager)
+    : stats_(
+          parent_stats ? parent_stats->create_child("Subarray") :
+                         storage_manager ?
+                         storage_manager->stats()->create_child("subSubarray") :
+                         nullptr)
+    , logger_(logger->clone("LabelledSubarray", ++logger_id_))
+    , coalesce_ranges_(coalesce_ranges)
+    , subarray_(array, layout, stats_, logger_, coalesce_ranges)
+    , label_subarrays_(array->array_schema_latest().dim_num(), nullopt) {
+  if (!parent_stats && !storage_manager)
+    throw std::runtime_error(
+        "Subarray(): missing parent_stats requires live storage_manager!");
+}
+
+Status LabelledSubarray::add_range(
+    unsigned dim_idx, const void* start, const void* end, const void* stride) {
+  if (is_labelled(dim_idx))
+    return label_subarrays_[dim_idx]->subarray.add_range(0, start, end, stride);
+  return subarray_.add_range(dim_idx, start, end, stride);
+}
+
+Status LabelledSubarray::add_point_ranges(
+    unsigned dim_idx, const void* start, uint64_t count) {
+  if (is_labelled(dim_idx))
+    return label_subarrays_[dim_idx]->subarray.add_point_ranges(
+        0, start, count);
+  return subarray_.add_point_ranges(dim_idx, start, count);
+}
+
+Status LabelledSubarray::add_range_var(
+    unsigned dim_idx,
+    const void* start,
+    uint64_t start_size,
+    const void* end,
+    uint64_t end_size) {
+  if (is_labelled(dim_idx))
+    return label_subarrays_[dim_idx]->subarray.add_range_var(
+        0, start, start_size, end, end_size);
+  return subarray_.add_range_var(dim_idx, start, start_size, end, end_size);
+}
+
+Status LabelledSubarray::get_range_num(
+    uint32_t dim_idx, uint64_t* range_num) const {
+  if (is_labelled(dim_idx))
+    return label_subarrays_[dim_idx]->subarray.get_range_num(0, range_num);
+  return subarray_.get_range_num(dim_idx, range_num);
+}
+
+Status LabelledSubarray::get_range(
+    unsigned dim_idx,
+    uint64_t range_idx,
+    const void** start,
+    const void** end,
+    const void** stride) const {
+  if (is_labelled(dim_idx))
+    return label_subarrays_[dim_idx]->subarray.get_range(
+        0, range_idx, start, end, stride);
+  return subarray_.get_range(dim_idx, range_idx, start, end, stride);
+}
+
+Status LabelledSubarray::get_range_var(
+    unsigned dim_idx, uint64_t range_idx, void* start, void* end) const {
+  if (is_labelled(dim_idx))
+    return label_subarrays_[dim_idx]->subarray.get_range_var(
+        0, range_idx, start, end);
+  return subarray_.get_range_var(dim_idx, range_idx, start, end);
+}
+
+Status LabelledSubarray::get_range_var_size(
+    uint32_t dim_idx,
+    uint64_t range_idx,
+    uint64_t* start_size,
+    uint64_t* end_size) const {
+  if (is_labelled(dim_idx))
+    return label_subarrays_[dim_idx]->subarray.get_range_var_size(
+        0, range_idx, start_size, end_size);
+  return subarray_.get_range_var_size(dim_idx, range_idx, start_size, end_size);
+}
+
+Status LabelledSubarray::set_external_label(
+    const unsigned dim_idx,
+    const std::string& label_name,
+    const std::string& internal_label_name,
+    const std::string& internal_index_name,
+    const Array* array) {
+  if (is_labelled(dim_idx))
+    return Status_LabelledSubarrayError(
+        "Cannot set label on dimension " + std::to_string(dim_idx) +
+        "; Dimension label is already set.");
+  if (subarray_.is_set(dim_idx))
+    return Status_LabelledSubarrayError(
+        "Cannot set label on dimension " + std::to_string(dim_idx) +
+        "; The dimension already has ranges set.");
+  label_subarrays_[dim_idx] = AxisSubarray(
+      label_name,
+      internal_label_name,
+      internal_index_name,
+      array,
+      subarray_.layout(),
+      stats_,
+      logger_,
+      coalesce_ranges_);
+  return Status::Ok();
+}
+
+Status LabelledSubarray::set_label(
+    const unsigned dim_idx, const unsigned label_idx) {
+  if (is_labelled(dim_idx))
+    return Status_LabelledSubarrayError(
+        "Cannot set label on dimension " + std::to_string(dim_idx) +
+        "; Dimension label is already set.");
+  if (subarray_.is_set(dim_idx))
+    return Status_LabelledSubarrayError(
+        "Cannot set label on dimension " + std::to_string(dim_idx) +
+        "; The dimension already has ranges set.");
+  return Status_LabelledSubarrayError(
+      "Cannot set label " + std::to_string(label_idx) + " on dimension " +
+      std::to_string(dim_idx) +
+      ". Support for internal labels is not yet implemented.");
+}
+
+Status LabelledSubarray::set_config(const Config& config) {
+  RETURN_NOT_OK(subarray_.set_config(config));
+  for (auto& label_subarray : label_subarrays_) {
+    if (label_subarray.has_value()) {
+      RETURN_NOT_OK(label_subarray->subarray.set_config(config));
+    }
+  }
+  return Status::Ok();
+}
+
+const Config* LabelledSubarray::config() const {
+  return subarray_.config();
+}
+
+Status LabelledSubarray::set_coalesce_ranges(bool coalesce_ranges) {
+  RETURN_NOT_OK(subarray_.set_coalesce_ranges(coalesce_ranges));
+  for (auto& label_subarray : label_subarrays_) {
+    if (label_subarray.has_value()) {
+      RETURN_NOT_OK(
+          label_subarray->subarray.set_coalesce_ranges(coalesce_ranges));
+    }
+  }
+  return Status::Ok();
+}
+
+}  // namespace tiledb::sm
---
+++ tiledb/sm/label_query/label_subarray.h
@@ -0,0 +1,227 @@
+#ifndef TILEDB_LABEL_SUBARRAY_H
+#define TILEDB_LABEL_SUBARRAY_H
+
+#include <unordered_map>
+#include <vector>
+#include "tiledb/common/common.h"
+#include "tiledb/common/logger_public.h"
+#include "tiledb/sm/subarray/subarray.h"
+
+using namespace tiledb::common;
+
+namespace tiledb::sm {
+
+class StorageManager;
+
+/** Return a Status_ArrayLabelsError error class Status with a given message **/
+inline Status Status_LabelledSubarrayError(const std::string& msg) {
+  return {"[TileDB::LabelledSubarray] Error", msg};
+}
+
+class AxisSubarray {
+ public:
+  AxisSubarray(
+      const std::string& label_name,
+      const std::string& internal_label_name,
+      const std::string& internal_index_name,
+      const Array* array,
+      const Layout layout,
+      stats::Stats* stats,
+      shared_ptr<Logger> logger,
+      const bool coalesce_ranges);
+
+  inline tuple<std::string, std::string, std::string> access_signature() {
+    return {label_name_, internal_label_name_, internal_index_name_};
+  }
+
+  Subarray subarray;
+
+ private:
+  std::string label_name_;
+  std::string internal_label_name_;
+  std::string internal_index_name_;
+};
+
+class LabelledSubarray {
+ public:
+  /** Default constructor is not C.41 compliant. */
+  LabelledSubarray() = delete;
+
+  /**
+   * Constructor.
+   *
+   * @param array The array the subarray is associated with.
+   * @param parent_stats The parent stats to inherit from.
+   * @param logger The parent logger to clone and use for logging
+   * @param coalesce_ranges When enabled, ranges will attempt to coalesce
+   *     with existing ranges as they are added.
+   */
+  LabelledSubarray(
+      const Array* array,
+      stats::Stats* parent_stats,
+      shared_ptr<Logger> logger,
+      bool coalesce_ranges = true,
+      StorageManager* storage_manager = nullptr);
+
+  /**
+   * Constructor.
+   *
+   * @param array The array the subarray is associated with.
+   * @param layout The layout of the values of the subarray (of the results
+   *     if the subarray is used for reads, or of the values provided
+   *     by the user for writes).
+   * @param parent_stats The parent stats to inherit from.
+   * @param logger The parent logger to clone and use for logging
+   * @param coalesce_ranges When enabled, ranges will attempt to coalesce
+   *     with existing ranges as they are added.
+   */
+  LabelledSubarray(
+      const Array* array,
+      Layout layout,
+      stats::Stats* parent_stats,
+      shared_ptr<Logger> logger,
+      bool coalesce_ranges = true,
+      StorageManager* storage_manager = nullptr);
+
+  /**
+   * Adds a range to the subarray on the input dimension by index,
+   * in the form of (start, end, stride).
+   * The range components must be of the same type as the domain type of the
+   * underlying array.
+   *
+   * This is called directly by the C-API.
+   *
+   * TODO: add params to docs.
+   */
+  Status add_range(
+      unsigned dim_idx, const void* start, const void* end, const void* stride);
+
+  /**
+   * Set point ranges from an array
+   *
+   * @param dim_idx Dimension index
+   * @param start Pointer to start of the array
+   * @param count Number of elements to add
+   * @return Status
+   */
+  Status add_point_ranges(unsigned dim_idx, const void* start, uint64_t count);
+
+  /**
+   * Adds a variable-sized range to the (read/write) query on the input
+   * dimension by index, in the form of (start, end).
+   */
+  Status add_range_var(
+      unsigned dim_idx,
+      const void* start,
+      uint64_t start_size,
+      const void* end,
+      uint64_t end_size);
+
+  /** Retrieves the number of ranges on the given dimension index. */
+  Status get_range_num(uint32_t dim_idx, uint64_t* range_num) const;
+
+  /**
+   * Retrieves a range from a dimension index in the form (start, end, stride).
+   *
+   * @param dim_idx The dimension to retrieve the range from.
+   * @param range_idx The id of the range to retrieve.
+   * @param start The range start to retrieve.
+   * @param end The range end to retrieve.
+   * @param stride The range stride to retrieve.
+   * @return Status
+   */
+  Status get_range(
+      unsigned dim_idx,
+      uint64_t range_idx,
+      const void** start,
+      const void** end,
+      const void** stride) const;
+
+  /**
+   * Retrieves a range from a variable-length dimension index in the form
+   * (start, end).
+   *
+   * @param dim_idx The dimension to retrieve the range from.
+   * @param range_idx The id of the range to retrieve.
+   * @param start The range start to retrieve.
+   * @param end The range end to retrieve.
+   * @return Status
+   */
+  Status get_range_var(
+      unsigned dim_idx, uint64_t range_idx, void* start, void* end) const;
+
+  /**
+   * Retrieves a range's start and end size for a given variable-length
+   * dimensions at a given range index.
+   *
+   *
+   * @param dim_idx The dimension to retrieve the range from.
+   * @param range_idx The id of the range to retrieve.
+   * @param start_size range start size in bytes
+   * @param end_size range end size in bytes
+   * @return Status
+   */
+  Status get_range_var_size(
+      uint32_t dim_idx,
+      uint64_t range_idx,
+      uint64_t* start_size,
+      uint64_t* end_size) const;
+
+  /**
+   * TODO Add documentation
+   */
+  inline bool is_labelled(const unsigned dim_idx) const {
+    return label_subarrays_.at(dim_idx).has_value();
+  }
+
+  /**
+   * TODO Add documentation
+   */
+  Status set_external_label(
+      const unsigned dim_idx,
+      const std::string& label_name,
+      const std::string& internal_label_name,
+      const std::string& internal_index_name,
+      const Array* array);
+
+  /**
+   * TODO Add documentation
+   */
+  Status set_label(const unsigned dim_idx, const unsigned label_idx);
+
+  // /**
+  //  * TODO Add documentation
+  //  */
+  // Status set_label_by_name(label_name);
+
+  /**
+   * Sets config for query-level parameters only.
+   *
+   * Part of the C-API.
+   **/
+  Status set_config(const Config& config);
+
+  /**
+   * Get the config of the writer
+   * @return Config
+   */
+  const Config* config() const;
+
+  /**
+   * Sets coalesce_ranges flag, intended for use by CAPI, to alloc matching
+   * default coalesce-ranges=true semantics of internal class constructor, but
+   * giving capi clients ability to turn off if desired.
+   */
+  Status set_coalesce_ranges(bool coalesce_ranges = true);
+
+ private:
+  stats::Stats* stats_;
+  inline static std::atomic<uint64_t> logger_id_ = 0;
+  shared_ptr<Logger> logger_;
+  bool coalesce_ranges_;
+  Subarray subarray_;
+  std::vector<optional<AxisSubarray>> label_subarrays_;
+};
+
+}  // namespace tiledb::sm
+#endif
